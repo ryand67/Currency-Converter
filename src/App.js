03dd358dc2b15132cb9ba502641fd52739979c45
ryand67 <rwdevenney@gmail.com>
2020-08-15 11:09:52 -0400

    functioning

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 import Dropdown from 'react-bootstrap/Dropdown';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Button from 'react-bootstrap/Button';
-import FormControl from 'react-bootstrap/FormControl';
+import axios from 'axios';
 
 function App() {
 
   const [baseCurrency, setBase] = useState('USD');
+  const [targetCurrency, setTarget] = useState('USD');
   const [convertAmount, setAmount] = useState(0);
+  const [convertAnswer, setAnswer] = useState(0);
 
-  const currencyList = ['USD', 'EUR', 'CAD'];
+  const currencyList = ['USD', 'EUR', 'CAD', 'GBP', 'JPY'];
 
   const handleAmountChange = (e) => {
     setAmount(e.target.value);
+  }
+
+  const handleConvert = () => {
+    axios.get(`https://api.exchangeratesapi.io/latest?base=${baseCurrency}&symbols=${targetCurrency}`)
+    .then(res => {
+      let obj = res.data.rates;
+      let rate = obj[Object.keys(obj)[0]];
+      let newAmount = convertAmount * rate;
+      newAmount = newAmount.toFixed(2);
+      setAnswer(newAmount);
+    });
   }
 
   return (
@@ -36,21 +47,22 @@ function App() {
 
         <Dropdown>
           <Dropdown.Toggle variant="success" id="dropdown-basic">
-            {baseCurrency}
+            {targetCurrency}
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
             {currencyList.map(item => {
-              return <Dropdown.Item key={item} onClick={() => setBase(item)}>{item}</Dropdown.Item>
+              return <Dropdown.Item key={item} onClick={() => setTarget(item)}>{item}</Dropdown.Item>
             })}
           </Dropdown.Menu>
         </Dropdown>
       </div>
 
       <div className="inputDiv">
-        <button className="convertButton">Convert</button>
+        <button className="convertButton" onClick={() => handleConvert()}>Convert</button>
         <input className="convertInput" placeholder="0" type="number" onChange={(e) => handleAmountChange(e)} />
       </div>
+          <h1 className="convertedAmount">{convertAnswer} {targetCurrency}</h1>
     </div>
   );
 }
