46a8362e8c3bb1e76666341a2fac44d95e03f2fc
ryand67 <rwdevenney@gmail.com>
2020-08-14 23:32:55 -0400

    inputs and dropdowns startd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
 
 function App() {
+
+  const [baseCurrency, setBase] = useState('USD');
+  const [convertAmount, setAmount] = useState(0);
+
+  const currencyList = ['USD', 'EUR', 'CAD'];
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="dropdownDiv">
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            {baseCurrency}
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            {currencyList.map(item => {
+              return <Dropdown.Item key={item} onClick={() => setBase(item)}>{item}</Dropdown.Item>
+            })}
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <i className="fas fa-arrow-right"></i>
+
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            {baseCurrency}
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            {currencyList.map(item => {
+              return <Dropdown.Item key={item} onClick={() => setBase(item)}>{item}</Dropdown.Item>
+            })}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+
+      <div className="inputDiv">
+        <button className="convertButton">Convert</button>
+        <input className="convertInput" placeholder="0" type="number" onChange={(e) => handleAmountChange(e)} />
+      </div>
     </div>
   );
 }
